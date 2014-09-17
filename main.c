0e1bf664b4e4d546ee7512b069650e1a5fb3a15d
wallecnik <tomas.valka@gmail.com>
2014-09-17 09:13:48 +0200

    Generated file

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Wallecnik
+ *
+ * Created on 16. září 2014, 12:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return (EXIT_SUCCESS);
+}
+
